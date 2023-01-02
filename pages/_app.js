96e90b05861dcd38d2e6510acbc74fe5c141be2f
Carolina Valladares <carolinalimacvalladares@gmail.com>
2023-01-01 21:17:26 -0300

    add toast css file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function App({ Component, pageProps }) {
   return (
